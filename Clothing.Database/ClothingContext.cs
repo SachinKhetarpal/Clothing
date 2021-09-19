2b7007a59395323e3d39ed3b456aa601c525fc6f
SachinKhetarpal <sachin0993@gmail.com>
2021-09-19 17:05:35 +0530

    Add project files.

---
+++ Clothing.Database/ClothingContext.cs
@@ -0,0 +1,20 @@
+﻿using Clothing.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clothing.Database
+{
+    public class ClothingContext : DbContext
+    {
+        public ClothingContext() : base("ClothingConnection")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Clothing.Web/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Clothing.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clothing.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            return View();
+        }
+    }
+}
