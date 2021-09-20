dd9356914a57f27a33357cb245e3d7d602c425a6
SachinKhetarpal <sachin0993@gmail.com>
2021-09-21 01:08:17 +0530

    applied jquery and added Product page

---
+++ Clothing.Services/ProductsService.cs
@@ -0,0 +1,56 @@
+﻿using Clothing.Database;
+using Clothing.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clothing.Services
+{
+    public class ProductsServices
+    {
+        public List<Product> GetProducts()
+        {
+            using (var context = new ClothingContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public Product GetProduct(int ID)
+        {
+            using (var context = new ClothingContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+
+        public void SaveProduct(Product product)
+        {
+            using (var context = new ClothingContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new ClothingContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            using (var context = new ClothingContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Clothing.Web/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Clothing.Entity;
+using Clothing.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clothing.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsServices productsService = new ProductsServices();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductTable(string Search)
+        {
+            var products = productsService.GetProducts();
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
